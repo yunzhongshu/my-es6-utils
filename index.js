96f2df15a21951a717027497c494dc953b12ae53
zhangyunyun <yunzhongshu_1@163.com>
2017-10-08 08:34:31 +0800

    build

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 /**
  * 格式化日期
  * @param fmt
  * @param date
  * @returns {*}
  */
-export const formatDate = (fmt, date) => {
+var formatDate = exports.formatDate = function formatDate(fmt, date) {
   if (!(date instanceof Date)) {
-    return date
+    return date;
   }
-  const o = {
+  var o = {
     'M+': date.getMonth() + 1,
     'd+': date.getDate(),
     'h+': date.getHours(),
@@ -16,14 +21,14 @@ export const formatDate = (fmt, date) => {
     's+': date.getSeconds(),
     'q+': Math.floor((date.getMonth() + 3) / 3),
     'S': date.getMilliseconds()
-  }
+  };
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (`${date.getFullYear()}`).substr(4 - RegExp.$1.length))
+    fmt = fmt.replace(RegExp.$1, ('' + date.getFullYear()).substr(4 - RegExp.$1.length));
   }
-  for (let k in o) {
-    if (new RegExp(`(${k})`).test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (`00${o[k]}`.substr(`${o[k]}`.length)))
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
     }
   }
-  return fmt
-}
+  return fmt;
+};
